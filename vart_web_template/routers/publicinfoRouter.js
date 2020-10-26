0a95c1fc7a493e54b611c2ebd4005cb7a4a59989
lee sang ho <kigkm@naver.com>
2020-10-26 15:01:49 +0900

    yes

--- vart_web_template/routers/publicinfoRouter.js
+++ vart_web_template/routers/publicinfoRouter.js
@@ -2,6 +2,46 @@ const router = require("express").Router();
 const transaction = require('./transaction')
 const Publicinfo = require('../schemas/publicinfo')
 const authenticate = require('../authenticate')
+
+const selectProperties = (object, projection) => {
+  let isProjection = false
+
+  for (let value of Object.values(projection)) {
+    isProjection = isProjection | value
+  }
+
+
+  if (isProjection) {
+    for (let objKey of Object.keys(object)) {
+      let isKey = false
+
+      for (let [proKey, proValue] of Object.entries(projection)) {
+        if (objKey === proKey) {
+          if (proValue) {
+            isKey = true
+          } else {
+            delete object[objKey]
+          }
+        }
+      }
+
+      if (!isKey) {
+        delete object[objKey]
+      }
+    }
+  } else {
+    for (let objKey of Object.keys(object)) {
+      for (let [proKey, proValue] of Object.entries(projection)) {
+        if (objKey === proKey) {
+          if (!proValue) {
+            delete object[objKey]
+          }
+        }
+      }
+    }
+  }
+}
+
 //조회
 router.get("/query/:infoKey", async (req, res) => {
   const { infoKey } = req.params;
@@ -26,7 +66,7 @@ router.get("/query/:infoKey", async (req, res) => {
 });
 
 //전체 조회
-router.get("/queryAll", async (req, res) => {
+router.get("/list", async (req, res) => {
   var data = {
     contract: await transaction.wallet(),
     function: "readAllPublicinfo",
@@ -37,14 +77,27 @@ router.get("/queryAll", async (req, res) => {
   if (result.result) {
     const show = result.data.toString('utf-8') // string(JSON)
     const realresult = JSON.parse(show) // object
+
+    const projection = {
+
+      Developerleader: 0,
+      Executive: 0
+
+
+    }
+
+    for (let info of realresult) {
+      console.log(info)
+      await selectProperties(info, projection)
+    }
+
     res.json(realresult);
+
   } else {
     console.log(`Error : ${result.data}`)
     res.status(401).json(JSON.parse(result.data))
   }
 });
-
-
 
 //공시 정보 입력(코인 이름, 코인 가격, 상장 여부 등등)
 router.post("/invoke", authenticate.company, async (req, res) => {
